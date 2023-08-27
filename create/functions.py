4811a69a15b209df8edb0ab7bf21a169ba514466
L3xTech <l3xtech@tuta.io>
2023-08-27 07:23:15 -0300

    function populate_list

--- create/functions.py
+++ create/functions.py
@@ -5,6 +5,20 @@ name_1 = ''
 name_2 = ''
 name_3 = ''
 
+def populate_list(key, primary_list, variable_to_insert):
+    secundary_list = []
+
+    for index in primary_list:
+        temp_dict = {}
+
+        for index_key, index_value in index.items():
+            new_index_value = input(f'Digite o {index_key} do novo contato: ')
+            temp_dict[index_key] = new_index_value
+
+        secundary_list.append(temp_dict)
+
+    variable_to_insert[key] = secundary_list
+
 # Cria um novo contato no db
 def create_new_contact(dicionario, lista):
     global balloon_dict
@@ -15,16 +29,7 @@ def create_new_contact(dicionario, lista):
     for chave, valor in dicionario.items():
 
         if isinstance(valor, list):
-            novo_valor = []
-            for item in valor:
-                novo_item = {}
-                for sub_chave, sub_valor in item.items():
-                    novo_sub_valor = input(f'Digite o {sub_chave} do novo contato: ')
-                    novo_item[sub_chave] = novo_sub_valor
-
-                novo_valor.append(novo_item)
-
-            balloon_dict[chave] = novo_valor
+            populate_list(chave, valor, balloon_dict)
 
         elif chave == 'id':
             balloon_dict[chave] = next(id_generator)
@@ -67,6 +72,7 @@ def create_new_contact(dicionario, lista):
     name_1 = ''
     name_2 = ''
     name_3 = ''
+
 
 
 def show_list_of_contacts(cont_list_db):
